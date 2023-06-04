fedb8198fce0996d67d7d210d0419db05f9e48bb
buggyj <buggyjef@gmail.com>
2023-06-04 08:32:25 +0200

    progress

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+let homeTab="";
+
 function setupContextMenu() {
   chrome.contextMenus.create({
     id: 'connect',
@@ -12,18 +14,39 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((data, tab) => {
 	console.log("url",tab.url);
-
-
-  chrome.storage.local.set({'conected': tab.url, 'homeTab':tab.id}, function() {
-	  console.log("connected var set");
-	  //chrome.runtime.sendMessage({
-    //name: 'doit'
-  //});  
-	  
-	  });
+	homeTab = tab.id;
+	chrome.storage.local.set({'conected': tab.url, 'homeTab':tab.id}, function() {
+		console.log("connected var set");
+		browser.sidebarAction.getPanel({}).then(setPanel);
+		});
   
 });
+function setPanel(oldPanel) {
+	browser.sidebarAction.setPanel({panel: (oldPanel==browser.runtime.getURL("/sidepanel.html"))
+		?browser.runtime.getURL("/sidepanelb.html")
+		:browser.runtime.getURL("/sidepanel.html")});
+}
 
+browser.browserAction.onClicked.addListener(function(){browser.sidebarAction.toggle()});
+/*
+let thisPanel = browser.runtime.getURL("/this.html");
+let thatPanel = browser.runtime.getURL("/that.html");
 
-browser.browserAction.onClicked.addListener(function(){  
-browser.sidebarAction.toggle()});
+function toggle(panel) {
+  if (panel === thisPanel) {
+    browser.sidebarAction.setPanel({panel: thatPanel});
+  } else {
+    browser.sidebarAction.setPanel({panel: thisPanel});
+  }
+}
+
+browser.browserAction.onClicked.addListener(() => {
+  browser.sidebarAction.getPanel({}).then(toggle);
+});
+*/
+function tabclose(tabId,changeInfo) {if (tabId == homeTab) browser.sidebarAction.setPanel({panel: browser.runtime.getURL("/startsidepanel.html")});}
+function tabchange(tabId,changeInfo) {
+	if (tabId == homeTab && changeInfo.status)	browser.sidebarAction.setPanel({panel: browser.runtime.getURL("/startsidepanel.html")});
+}
+chrome.tabs.onRemoved.addListener(tabclose);
+chrome.tabs.onUpdated.addListener(tabchange);
--- sidepanel.js
+++ sidepanel.js
@@ -20,6 +20,8 @@ var current= {};
 current.url  = null;
 var tags;
 var taglist = [];
+var homeTab = "";
+
 
 function restorTags() {
 	tags = unedittags;
@@ -106,21 +108,19 @@ var getTid = function() {
 	document.querySelector('#cancel').style.display = "block";
 	browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	  var currUrl = tabs[0].url;
-		browser.storage.local.get({homeTab:""}, function(items){  
-			browser.tabs.sendMessage(items.homeTab, {action : 'getTid', data:{url:currUrl}, opts:"edit"},function(res){
-				let y = document.querySelector('#content');
-				let x = document.querySelector('#edit');
-				y.style.display = "none";
-				taglist = (JSON.parse(res.aux)).taglist;
-				puttags(res.data,true);
-				//console.log("res.data ",res.data);
-				x.value=(JSON.parse(res.data)).text;
-				x.style.display = "block";
-				x.dispatchEvent(new Event("input"))
+		browser.tabs.sendMessage(homeTab, {action : 'getTid', data:{url:currUrl}, opts:"edit"},function(res){
+			let y = document.querySelector('#content');
+			let x = document.querySelector('#edit');
+			y.style.display = "none";
+			taglist = (JSON.parse(res.aux)).taglist;
+			puttags(res.data,true);
+			//console.log("res.data ",res.data);
+			x.value=(JSON.parse(res.data)).text;
+			x.style.display = "block";
+			x.dispatchEvent(new Event("input"))
 
-			});
 		});
-	});
+});
 }
 
 
@@ -132,7 +132,8 @@ document.querySelector('#get').addEventListener('click', getTid);
 document.querySelector('#put').addEventListener('click', function() {
 	let x = document.querySelector('#edit')
 	let y = document.querySelector('#content');
-	document.querySelector('#get').style.display = "block";
+	let but =	document.querySelector('#get');
+	but.style.display = "block"
 	document.querySelector('#put').style.display = "none";
 	document.querySelector('#cancel').style.display = "none";
 	//console.log("emptybox ");
@@ -142,26 +143,28 @@ document.querySelector('#put').addEventListener('click', function() {
 							
 		browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		  var currUrl = tabs[0].url;
-		  	browser.tabs.sendMessage( tabs[0].id, {action : 'getMeta'}, function (meta)	{ 
-				console.log("meta",meta.description,meta.mediaImage);
-			browser.storage.local.get({homeTab:""}, function(items){  
-				browser.tabs.sendMessage(items.homeTab, {action : 'putTid', 
-					data:{url:currUrl, tags:JSON.stringify(tags),description:meta.description, mediaImage:meta.mediaImage, text:x.value, title:tabs[0].title, favIconUrl:tabs[0].favIconUrl}, opts:"put"},function(res){
+				current.text = x.value;
+				current.tags = JSON.stringify(tags);
+				browser.tabs.sendMessage(homeTab, {action : 'putTid', data:current, opts:"put"},function(res){
 					//console.log(res);
-					browser.tabs.sendMessage(items.homeTab, {action : 'getTid', data:{url:currUrl},opts:"render"},function(res){
-						y.innerHTML=(JSON.parse(res.data)).text;
-						taglist = (JSON.parse(res.aux)).taglist;
+					browser.tabs.sendMessage(homeTab, {action : 'getTid', data:{url:currUrl},opts:"render"},function(res){
+						let data = JSON.parse(res.data); 
+						let aux = JSON.parse(res.aux);
+						console.log("res.aux ",res.aux);
+						taglist = aux.taglist;
+						y.innerHTML=data.text;
 						puttags(res.data);
+						if (aux.new=='true') but.style.background='cyan';
+						else but.style.background='red';
 						y.style.display = "block";
-						x.value = "";
 						//console.log("reloaded new");
 					});
 					
 					
 					//console.log("res.data ",res.data);(document.querySelector('#content')).innerHTML=res.data;
 				});
-			});
-		});
+
+
 	});
 });	
 
@@ -180,12 +183,49 @@ document.querySelector('#cancel').addEventListener('click', function() {
 	restorTags();
 });	
 //-----------------------------
+function pullNote(tabs) {
+	let x = document.querySelector('#edit');
+	let y = document.querySelector('#content');
+	let z = document.querySelector('#icon');
+	let but =	document.querySelector('#get');
+	but.style.display = "block"
+	document.querySelector('#put').style.display = "none";
+	document.querySelector('#cancel').style.display = "none";
+	let editvalue = x.value;
+	x.value = "";//this stops multiple calls to save 
+	x.style.display = "none";
+			browser.tabs.sendMessage(tabs[0].id, {action : 'getMeta'}, function (meta)	{ 
+			if (meta){	console.log("meta",meta.description,meta.mediaImage);
+				current.mediaImage=meta.mediaImage;
+				current.description=meta.description;
+			} else console.log ("meta not defined")
+			current.url = tabs[0].url;
+			current.favIconUrl=tabs[0].favIconUrl;
+			current.title=tabs[0].title;
+			current.text = "";//edited text
+			current.tags = [];//edited values
+			browser.tabs.sendMessage(homeTab, {action : 'getTid',data:{url:current.url} ,opts:"render"},function(res){
+				let data = JSON.parse(res.data); 
+				let aux = JSON.parse(res.aux);
+				console.log("res.aux ",res.aux);
+				taglist = aux.taglist;
+				y.innerHTML=data.text;
+				puttags(res.data);
+				if (aux.new=='true') but.style.background='cyan';
+				else but.style.background='red';
+				y.style.display = "block";
+				
+			});
+	  });
 
+  //console.log(tabs[0].url);
+}
 function getNote(tabs) {
 	let x = document.querySelector('#edit');
 	let y = document.querySelector('#content');
 	let z = document.querySelector('#icon');
-	document.querySelector('#get').style.display = "block";
+	let but =	document.querySelector('#get');
+	but.style.display = "block"
 	document.querySelector('#put').style.display = "none";
 	document.querySelector('#cancel').style.display = "none";
 	let editvalue = x.value;
@@ -195,30 +235,37 @@ function getNote(tabs) {
  
 
 		browser.storage.local.get({homeTab:""}, function(items){  
+		//first put old values
 		  current.text = editvalue;
 		  current.tags = JSON.stringify(tags);
-			if (current.url && editvalue) browser.tabs.sendMessage(items.homeTab, {action : 'putTid', opts:"puts",data:current},function(res){
+			if (current.url && editvalue) browser.tabs.sendMessage(homeTab, {action : 'putTid', opts:"puts",data:current},function(res){
 				//console.log(res);
 			});
+			//get new url/tab data
 			browser.tabs.sendMessage(tabs[0].id, {action : 'getMeta'}, function (meta)	{ 
-				console.log("meta",meta.description,meta.mediaImage);
-			current.mediaImage=meta.mediaImage;
-			current.description=meta.description;
-			current.url = tabs[0].url;
-			current.favIconUrl=tabs[0].favIconUrl;
-			current.title=tabs[0].title;
-			current.text = "";
-			current.tags = [];
-			//z.src=tabs[0].favIconUrl;
-			browser.tabs.sendMessage(items.homeTab, {action : 'getTid',data:{url:current.url} ,opts:"render"},function(res){
+				if (meta){	console.log("meta",meta.description,meta.mediaImage);
+					current.mediaImage=meta.mediaImage;
+					current.description=meta.description;
+				} else console.log ("meta not defined")
+				current.url = tabs[0].url;
+				current.favIconUrl=tabs[0].favIconUrl;
+				current.title=tabs[0].title;
+				current.text = "";
+				current.tags = [];
+				//z.src=tabs[0].favIconUrl;
+				browser.tabs.sendMessage(items.homeTab, {action : 'getTid',data:{url:current.url} ,opts:"render"},function(res){
+				let data = JSON.parse(res.data); 
+				let aux = JSON.parse(res.aux);
 				console.log("res.aux ",res.aux);
-				taglist = (JSON.parse(res.aux)).taglist;
-				y.innerHTML=(JSON.parse(res.data)).text;
+				taglist = aux.taglist;
+				y.innerHTML=data.text;
 				puttags(res.data);
+				if (aux.new=='true') but.style.background='cyan';
+				else but.style.background='red';
 				y.style.display = "block";
-				
-			});
-	  });
+					
+				});
+		  });
   });
   //console.log(tabs[0].url);
 }
@@ -232,9 +279,6 @@ function onError(error) {
   console.error(`Error: ${error}`);
 }
 
-browser.tabs
-  .query({ currentWindow: true, active: true })
-  .then(getNote, onError);
 
 
  
@@ -249,34 +293,50 @@ browser.runtime.onMessage.addListener(({ name, data }) => {
   }
 });
 
-browser.tabs.onActivated.addListener((actTabInfo) => {
-  browser.tabs.get(actTabInfo.tabId, (tab) => {
-	  browser.windows.getCurrent({} ,function(Window) {//console.log("onActivated");
-		if (actTabInfo.windowId == Window.id) getNote([tab]);
-	});
+	
+browser.tabs.onActivated.addListener((actTabInfo) => {console.log("onActivated1");
+	if (actTabInfo.windowId == WindowId) browser.tabs.query({active: true, currentWindow: true, status:'complete'}, function(tabs) {
+			console.log("onActivated");
+			if (tabs.length > 0) getNote(tabs);
+
   });
 });
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.active && (changeInfo.url || changeInfo.status)) {//console.log("onUpdated");
-	  browser.windows.getCurrent({} ,function(Window) { 
-		if (tab.windowId == Window.id) getNote([tab]);
-	});
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {console.log("onUpdated1");
+  if (tab.active && ( changeInfo.status == "complete")) {
+ 
+		if (tab.windowId == WindowId) {
+			console.log("onUpdated",tab.windowId,"==",WindowId,"::",changeInfo.status,":url",changeInfo.url);
+			getNote([tab]);
+		}
+
   }
 });
 
 
+window.addEventListener('unload', function() {
+	let x = document.querySelector('#edit')
+				current.text = x.value;
+				current.tags = JSON.stringify(tags);
+				browser.tabs.sendMessage(homeTab, {action : 'putTid', data:current, opts:"put"},function(res){
+					//console.log(res)	
+
+	});
+});	
+
+var WindowId =null;
 
 
-
-main();
 function main(){
-	browser.storage.local.get({conected:"not here"}, function(items){
-				document.querySelector('#home').innerHTML = items.conected;
-		} 
+	browser.windows.getCurrent({} ,function(Window) {console.log("window is",Window.id);
+		WindowId = Window.id;
+	});
+	browser.storage.local.get({conected:"not here",homeTab:""}, function(items){
+		document.querySelector('#home').innerHTML = items.conected;
+		homeTab = items.homeTab;
+		browser.tabs
+		  .query({ currentWindow: true, active: true })
+		  .then(pullNote, onError);
 
-
-	);
-
-	
-	
+	});
 }
+main();
